a03609f557fb494e2db2961f4095903280943cb9
Perpydev <perpetual@pledre.com>
2023-11-02 04:39:15 +0100

    git cache

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(express.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Listening on port 8080');
+})
