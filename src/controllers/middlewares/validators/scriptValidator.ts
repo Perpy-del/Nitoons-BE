2922f01f592c1799e395449697fa44874d4e8b02
Perpydev <perpetual@pledre.com>
2023-11-17 14:52:45 +0100

    formatting changes

--- src/controllers/middlewares/validators/scriptValidator.ts
+++ src/controllers/middlewares/validators/scriptValidator.ts
@@ -1,105 +1,142 @@
-import Joi from 'joi';
-import { Request, Response, NextFunction } from 'express';
-
+import Joi from 'joi'
+import { Request, Response, NextFunction } from 'express'
 
 //Validate User Id
-export function validateUserId(req: Request, res: Response, next: NextFunction) {
-    const { user_id } = req.params;
+export function validateUserId(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { user_id } = req.params
 
-    const user_idSchema = Joi.string().pattern(new RegExp("^[0-9a-fA-F]{24}$")).required();
-    const validationResult = user_idSchema.validate(user_id);
+  const user_idSchema = Joi.string()
+    .pattern(new RegExp('^[0-9a-fA-F]{24}$'))
+    .required()
+  const validationResult = user_idSchema.validate(user_id)
 
-    if (validationResult.error) {
-        return res.status(400).json({ error: `${validationResult.error.details[0].message},` });
-      }
-    
-      next();
+  if (validationResult.error) {
+    return res
+      .status(400)
+      .json({ error: `${validationResult.error.details[0].message},` })
+  }
+
+  next()
 }
 
-
 //Validate User Id And  Script Id
-export function validateScriptId(req: Request, res: Response, next: NextFunction) {
-    const { user_id } = req.params;
-    const { script_id } = req.body; 
+export function validateScriptId(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { user_id } = req.params
+  const { script_id } = req.body
 
-    const script_idSchema = Joi.object({
-        user_id: Joi.string().pattern(new RegExp("^[0-9a-fA-F]{24}$")).required(),
-        script_id: Joi.string().pattern(new RegExp("^[0-9a-fA-F]{24}$")).required()
-    })
-    const validationResult = script_idSchema.validate({user_id, script_id});
+  const script_idSchema = Joi.object({
+    user_id: Joi.string().pattern(new RegExp('^[0-9a-fA-F]{24}$')).required(),
+    script_id: Joi.string().pattern(new RegExp('^[0-9a-fA-F]{24}$')).required(),
+  })
+  const validationResult = script_idSchema.validate({ user_id, script_id })
 
-    if (validationResult.error) {
-        return res.status(400).json({ error: `${validationResult.error.details[0].message},` });
-      }
-      next();
+  if (validationResult.error) {
+    return res
+      .status(400)
+      .json({ error: `${validationResult.error.details[0].message},` })
+  }
+  next()
 }
 
 //Validate User Id , Script Id And Script Title
-export function validateScriptTitle(req: Request, res: Response, next: NextFunction) {
-    const { user_id } = req.params;
-    const { script_id, title } = req.body; 
+export function validateScriptTitle(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { user_id } = req.params
+  const { script_id, title } = req.body
 
-    const script_idSchema = Joi.object({
-        user_id: Joi.string().pattern(new RegExp("^[0-9a-fA-F]{24}$")).required(),
-        script_id: Joi.string().pattern(new RegExp("^[0-9a-fA-F]{24}$")).required(),
-        title: Joi.string().required()
-    })
-    const validationResult = script_idSchema.validate({user_id, script_id, title});
+  const script_idSchema = Joi.object({
+    user_id: Joi.string().pattern(new RegExp('^[0-9a-fA-F]{24}$')).required(),
+    script_id: Joi.string().pattern(new RegExp('^[0-9a-fA-F]{24}$')).required(),
+    title: Joi.string().required(),
+  })
+  const validationResult = script_idSchema.validate({
+    user_id,
+    script_id,
+    title,
+  })
 
-    if (validationResult.error) {
-        return res.status(400).json({ error: `${validationResult.error.details[0].message},` });
-      }
-    
-      next();
+  if (validationResult.error) {
+    return res
+      .status(400)
+      .json({ error: `${validationResult.error.details[0].message},` })
+  }
+
+  next()
 }
-
-
 
 //Validate User Id And Array Of Script Ids
-export function validateScriptIds(req: Request, res: Response, next: NextFunction) {
-    const { user_id } = req.params;
-    const { scriptIds } = req.body; 
+export function validateScriptIds(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { user_id } = req.params
+  const { scriptIds } = req.body
 
-    const script_idSchema = Joi.object({
-        user_id: Joi.string().pattern(new RegExp("^[0-9a-fA-F]{24}$")).required(),
-        scriptIds: Joi.array().items(Joi.string().pattern(new RegExp("^[0-9a-fA-F]{24}$"))).required(),
-    });
-    const validationResult = script_idSchema.validate({user_id, scriptIds})
+  const script_idSchema = Joi.object({
+    user_id: Joi.string().pattern(new RegExp('^[0-9a-fA-F]{24}$')).required(),
+    scriptIds: Joi.array()
+      .items(Joi.string().pattern(new RegExp('^[0-9a-fA-F]{24}$')))
+      .required(),
+  })
+  const validationResult = script_idSchema.validate({ user_id, scriptIds })
 
-    if (validationResult.error) {
-        return res.status(400).json({ error: `${validationResult.error.details[0].message},` });
-      }
-    
-      next();
+  if (validationResult.error) {
+    return res
+      .status(400)
+      .json({ error: `${validationResult.error.details[0].message},` })
+  }
+
+  next()
 }
 
-export function validateScriptData(req: Request, res: Response, next: NextFunction) {
-    const { user_id } = req.params;
-    const scripts = req.body.scripts;
+export function validateScriptData(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { user_id } = req.params
+  const scripts = req.body.scripts
 
-    const ScriptSchema = Joi.object({
-        _id: Joi.string().required(),
-        title: Joi.string().default('undefined'),
-        isStarred: Joi.boolean().default(false),
-        isTrashed: Joi.boolean().default(false),
-        isDeleted: Joi.boolean().default(false),
-        user_id: Joi.string().pattern(new RegExp("^[0-9a-fA-F]{24}$")).required(),
-        created_at: Joi.date().required(),
-        updated_at: Joi.date().required(),
-        __v: Joi.number().required(),
-    });
+  const ScriptSchema = Joi.object({
+    _id: Joi.string().required(),
+    title: Joi.string().default('undefined'),
+    isStarred: Joi.boolean().default(false),
+    isTrashed: Joi.boolean().default(false),
+    isDeleted: Joi.boolean().default(false),
+    user_id: Joi.string().pattern(new RegExp('^[0-9a-fA-F]{24}$')).required(),
+    created_at: Joi.date().required(),
+    updated_at: Joi.date().required(),
+    __v: Joi.number().required(),
+  })
 
-    const schema = Joi.array().items(ScriptSchema);
-      const validationResult = schema.validate(scripts);
-      if (validationResult.error) {
-        return res.status(400).json({ error: `${validationResult.error.details[0].message} sam` }); 
-      }
-    
+  const schema = Joi.array().items(ScriptSchema)
+  const validationResult = schema.validate(scripts)
+  if (validationResult.error) {
+    return res
+      .status(400)
+      .json({ error: `${validationResult.error.details[0].message} sam` })
+  }
 
-      const userIdValidationResult = Joi.string().pattern(new RegExp("^[0-9a-fA-F]{24}$")).validate(user_id);
+  const userIdValidationResult = Joi.string()
+    .pattern(new RegExp('^[0-9a-fA-F]{24}$'))
+    .validate(user_id)
 
-      if (userIdValidationResult.error) {
-          return res.status(400).json({ error: userIdValidationResult.error.details[0].message });
-      }
-      next();
+  if (userIdValidationResult.error) {
+    return res
+      .status(400)
+      .json({ error: userIdValidationResult.error.details[0].message })
+  }
+  next()
 }
--- src/controllers/routers/scriptRouter.ts
+++ src/controllers/routers/scriptRouter.ts
@@ -1,17 +1,66 @@
-import express from "express";
-import { validateUserId, validateScriptIds,validateScriptId, validateScriptTitle, validateScriptData } from "../middlewares/validators/scriptValidator"
-import ScriptController from "../scriptController/ScriptController";
-import { authMiddleware } from "../middlewares/jwtHandler"
+import express from 'express'
+import {
+  validateUserId,
+  validateScriptIds,
+  validateScriptId,
+  validateScriptTitle,
+  validateScriptData,
+} from '../middlewares/validators/scriptValidator'
+import ScriptController from '../scriptController/ScriptController'
+import { authMiddleware } from '../middlewares/jwtHandler'
 
-export const scriptRouter = express.Router();
-scriptRouter.post('/create-script/:user_id', authMiddleware, validateUserId, ScriptController.createScript);
-scriptRouter.get('/fetch-scripts/:user_id', authMiddleware, validateUserId, ScriptController.getScriptsByUserId);
-scriptRouter.put('/update-scripts/:user_id', authMiddleware, validateScriptData, ScriptController.updateScripts);
-scriptRouter.put('/update-scripts/trash/:user_id', authMiddleware, validateScriptIds, ScriptController.updateScriptTrash);
-scriptRouter.put('/update-scripts/rename/:user_id', authMiddleware, validateScriptTitle, ScriptController.updateScriptName);
-scriptRouter.put('/update-scripts/trash-one/:user_id', authMiddleware, validateScriptId, ScriptController.trashScript);
-scriptRouter.put('/update-scripts/untrash-one/:user_id', authMiddleware, validateScriptId, ScriptController.removeTrashScript);
-scriptRouter.put('/update-scripts/duplicate-script/:user_id', authMiddleware, validateScriptId, ScriptController.duplicateCreatedScript);
-scriptRouter.put('/delete-scripts/delete-one/:user_id', validateScriptId, ScriptController.deleteScript);
+export const scriptRouter = express.Router()
+scriptRouter.post(
+  '/create-script/:user_id',
+  authMiddleware,
+  validateUserId,
+  ScriptController.createScript,
+)
+scriptRouter.get(
+  '/fetch-scripts/:user_id',
+  authMiddleware,
+  validateUserId,
+  ScriptController.getScriptsByUserId,
+)
+scriptRouter.put(
+  '/update-scripts/:user_id',
+  authMiddleware,
+  validateScriptData,
+  ScriptController.updateScripts,
+)
+scriptRouter.put(
+  '/update-scripts/trash/:user_id',
+  authMiddleware,
+  validateScriptIds,
+  ScriptController.updateScriptTrash,
+)
+scriptRouter.put(
+  '/update-scripts/rename/:user_id',
+  authMiddleware,
+  validateScriptTitle,
+  ScriptController.updateScriptName,
+)
+scriptRouter.put(
+  '/update-scripts/trash-one/:user_id',
+  authMiddleware,
+  validateScriptId,
+  ScriptController.trashScript,
+)
+scriptRouter.put(
+  '/update-scripts/untrash-one/:user_id',
+  authMiddleware,
+  validateScriptId,
+  ScriptController.removeTrashScript,
+)
+scriptRouter.put(
+  '/update-scripts/duplicate-script/:user_id',
+  authMiddleware,
+  validateScriptId,
+  ScriptController.duplicateCreatedScript,
+)
+scriptRouter.put(
+  '/delete-scripts/delete-one/:user_id',
+  validateScriptId,
+  ScriptController.deleteScript,
+)
 // scriptRouter.delete('/delete-scripts/:user_id', validateScriptId, deleteScriptsById);
- 
