9fe3592290317640b88b5d2e7d810187563c0c57
Admin <admin@Macs-MacBook-Pro.local>
2023-11-28 05:26:37 +0100

    fixes and changes to script

--- src/controllers/scriptController/ScriptController.ts
+++ src/controllers/scriptController/ScriptController.ts
@@ -56,13 +56,11 @@ export default class ScriptController {
   }
 
   public static async getOneScriptById(req: Request, res: Response) {
-    const { script_id } = req.body;
-    const { user_id } = req.params;
+    const { script_id } = req.params;
     try {
       const scriptId = new Types.ObjectId(script_id)
-      const userId = new Types.ObjectId(user_id)
 
-      const getScript = await ScriptNamespace.getOneScript({ script_id: scriptId, user_id: userId })
+      const getScript = await ScriptNamespace.getOneScript({ script_id: scriptId})
 
       return ResponseNamespace.sendSuccessMessage(
         res,
